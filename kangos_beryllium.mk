6c46193a86457d249bcf2e4d6e3e014643691e15
Bruno Martins <bgcngm@gmail.com>
2021-04-20 22:46:18 +0100

    beryllium: Update blobs from V12.0.3.0.QEJMIXM global stable
    Change-Id: I6d292bbeeb870c796ef80da6d8fffd94b917d5c0

--- kangos_beryllium.mk
+++ kangos_beryllium.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -25,7 +25,7 @@ PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium" \
     PRODUCT_BRAND="Xiaomi"
 
